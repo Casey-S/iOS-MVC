99bc81d4a40f6c31053850a2b2c4bf3308a88192
Casey-S <caseyspen@gmail.com>
2016-11-16 12:04:39 -0800

    Added latest Mood Tracker work.

--- 1102-w/MoodTracker-starter/MoodTracker-starter/View Controllers/FriendsTableViewController.swift
+++ 1102-w/MoodTracker-starter/MoodTracker-starter/View Controllers/FriendsTableViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class FriendsTableViewController: UITableViewController {
   
+    var friends: [Friend] = [
+        Friend(name: "Casey", mood: .happy),
+        Friend(name: "Cody", mood: .angry)
+    ]
+    
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return friends.count
   }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendTableViewCell") as! FriendTableViewCell
+        cell.friend = friends[indexPath.row]
+        
+        return cell
+    }
+    
 }
